bc251e0e15351410e5eb407ae1360000ce87d3d2
Feenis <willis_369@yahoo.com>
2014-12-01 16:33:49 -0800

    Added Pvp Shooter

---
+++ TopDownShooter/Assets/CharMovement2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMovement2 : MonoBehaviour {
+	public float speed;
+	public float rotatespeed;
+	public GameObject bullet;
+	private CharacterController player;
+	
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<CharacterController> ();
+	}
+	
+	
+	void Update () {
+		Vector3 movement = new Vector3(0,0,0);
+		
+		//Vertical Movement and Horizontal
+		if (Input.GetAxis("Forward2") > 0) 
+			movement = speed*transform.forward/5;
+		if (Input.GetAxis("Forward2") < 0)
+			movement = speed*transform.forward/5*-1;
+		
+		//Gravity
+		if (!player.isGrounded) 
+			movement.y = movement.y - .1f;
+		
+		//Shooting Mechanics
+		//bullet.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Rotate")*100);
+		if (Input.GetButtonDown ("Shoot2"))
+			Instantiate (bullet, new Vector3(transform.position.x,
+			                                 transform.position.y,
+			                                 transform.position.z), transform.rotation);
+		
+		player.Move (movement);
+		//Camera Movement -  note: must go after player movement
+		transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Rotate2")*rotatespeed);
+	}
+}
---
+++ TopDownShooter/Assets/MoveBullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBullet : MonoBehaviour {
+	public float speed;
+	private int death = 120;
+
+	void Start () {
+		if(Input.GetAxis("Forward") > 0)
+			transform.Translate (new Vector3(0,0,speed*1.75f));;
+	}
+	
+
+	void Update () {
+		death--;
+		//this might work as game mechanic (sidenote)
+		//transform.RotateAround (transform.position, Vector3.up, 45);
+		transform.Translate (new Vector3(0,0,speed));
+		if (death <= 0) 
+			Destroy (this.gameObject);
+	}
+}
