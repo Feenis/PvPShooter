0fb9e5590c26525af3f766d72c2d3d884e9a2b01
Feenis <willis_369@yahoo.com>
2014-12-05 11:19:49 -0800

    more power up mechanics, bullet speed, etc

--- TopDownShooter/Assets/CharMovement.cs
+++ TopDownShooter/Assets/CharMovement.cs
@@ -6,13 +6,17 @@ public class CharMovement : MonoBehaviour {
 	public float atkspeed;
 	public float dmg;
 	public float maxhealth;
-	public float rotatespeed;
+	public float maxdmg;
+	public float doubleatkspeed;							//power up
+	public float incrdmg;									//power up
+	public float incrspeed;									//power up
+	public float initspeed = 1f;							//initial speed
 	public GameObject bullet;
-	public GameObject cube2;
+	public GameObject cube2;								//used to deal with effects on enemy player
 	private CharacterController player;
+	private float rotatespeed = 5f;
 	private float currhealth;
-	private float initspeed = .5f;							//initial speed
-	private float currspeed;
+	private float currspeed = 3f;							//speed we can get to currently
 	private float maxatkspd = .2f;							//fastest the bullet can be rapidly shoty
 	private bool shot = false;								//checks if bullet already shot the player
 	private bool shooting = false;							//checks if player already shot bullet within attack speed period
@@ -29,32 +33,32 @@ public class CharMovement : MonoBehaviour {
 		shot = false;										//shot checking so it does not damage twice for one bullet
 
 		//Attack Speed Checking
-		if(atkspeed < maxatkspd)
-			atkspeed = maxatkspd;
+		atkspeed *= doubleatkspeed;							//if double attack speed power up is in use
 
 		//Forward Movement and Acceleration
-		if(currspeed > maxspeed)
-			currspeed = maxspeed;
 		if (Input.GetAxis("Forward") > 0) {
-			if(initspeed > currspeed)
+			if(initspeed > (currspeed+incrspeed))			//checks for increased movement power up
 				initspeed = currspeed;
-			else
-				initspeed += .05f;							//acceleration						
+			else 
+				initspeed += .04f;							//acceleration						
 			movement = initspeed*transform.forward/5;
 		}
 		if (Input.GetAxis("Forward") < 0) {
-			if(initspeed > currspeed)
+			if(initspeed > (currspeed+incrspeed))			//checks for increased movement power up
 				initspeed = currspeed;
 			else
-				initspeed += .05f;							//acceleration
+				initspeed += .04f;							//acceleration
 			movement = initspeed*transform.forward/5*-1;
 		}
 		if (Input.GetAxis("Forward") == 0)
-			initspeed = .5f;
+		{
+			if(initspeed > 1.5f)
+				initspeed -= .5f;
+		}
 		
 		//Gravity
 		if (!player.isGrounded) 
-			movement.y = movement.y - 10f;
+			movement.y = movement.y - 1f;
 		
 		//Shooting Mechanics
 		//bullet.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Rotate")*100);
@@ -77,30 +81,40 @@ public class CharMovement : MonoBehaviour {
 		//Got hit by bullet
 		if(hit.gameObject.name == "Bullet2(Clone)" && shot == false) 
 		{
-			float enemydmg = cube2.GetComponent<CharMovement2>().dmg;			//get enemy's damage
-			currhealth = currhealth - enemydmg;								//subtract from current health
+			float enemydmg = cube2.GetComponent<CharMovement2>().dmg +
+							 cube2.GetComponent<CharMovement2>().incrdmg;	    //get enemy's damage
+			currhealth = currhealth - enemydmg;									//subtract from current health
 			if(currhealth <= 0)
-				Destroy (this.gameObject);									//death
-			shot = true;													//enemy has been shot with this bullet already
+				Destroy (this.gameObject);										//death
+			shot = true;														//enemy has been shot with this bullet already
 		}
 	}
 
 	void OnCollisionEnter(Collider hit)
 	{
 		if(hit.gameObject.name == "attackup"){
-			dmg++;
+			if (dmg < maxdmg)
+				dmg++;
+			else
+				dmg = maxdmg;			
 		}
 		if(hit.gameObject.name == "attackdown") {
 			//cube2.GetComponent<CharMovement2>().lowerdmg();
 		}
 		if(hit.gameObject.name == "speedup") {
-			currspeed += .5f;
+			if(currspeed < maxspeed)
+				currspeed += .5f;
+			else
+				currspeed = maxspeed;
 		}
 		if(hit.gameObject.name == "speeddown") {
 			//cube2.GetComponent<CharMovement2>().lowerspeed();
 		}
 		if(hit.gameObject.name == "atkspeedup") {
-			atkspeed -= .1f;
+			if(atkspeed > maxatkspd)
+				atkspeed -= .1f;
+			else
+				atkspeed = maxatkspd;
 		}
 		if(hit.gameObject.name == "atkspeeddown") {
 			//cube2.GetComponent<CharMovement2>().loweratkspeed();
@@ -111,12 +125,52 @@ public class CharMovement : MonoBehaviour {
 			else
 				currhealth = maxhealth;
 		}
+		if(hit.gameObject.name == "doubleatkspeed") {
+			doubleatkspeed = 2;
+			StartCoroutine(BackToNormal(5));
+		}
+		if(hit.gameObject.name == "increasedDmg") {
+			incrdmg = 4;
+			StartCoroutine(BackToNormal(5));
+		}
+		if(hit.gameObject.name == "increasedSpd") {
+			incrspeed = 2;
+			StartCoroutine(BackToNormal(5));
+		}
 		Destroy (hit.gameObject);
 	}
 
+	public void lowerdmg()
+	{
+		if (dmg >= 2)
+			dmg--;
+	}
+
+	public void lowerspeed()
+	{
+		if (currspeed >= 1.5f)
+			currspeed -= .5f;
+	}
+
+	public void loweratkspeed()
+	{
+		if (atkspeed < 1)
+			atkspeed += .1f;
+	}
+
+	//keeps attack speed maintained
 	IEnumerator WaitFor(float seconds)
 	{
 		yield return new WaitForSeconds(seconds);
 		shooting = false;
+	}
+
+	//returns player back to normal after a specific power up
+	IEnumerator BackToNormal(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		incrdmg = 0;
+		incrspeed = 0;
+		doubleatkspeed = 1;
 	}
 }
