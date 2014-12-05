690f9e94c13b6a2a938292eb4d26741b711cfa59
brandonc91 <brandonc91@gmail.com>
2014-12-05 12:16:21 -0800

    creating new scene for main menu

---
+++ TopDownShooter/Assets/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	
+		void OnGUI ()
+		{
+				int xOffset = (Screen.width - 600) / 2;
+				int yOffset = (Screen.height - 400) / 2;
+				Rect pos = new Rect (xOffset, yOffset, 600, 25);
+		}
+}
