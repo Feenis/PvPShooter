fb991e2a3e9d3a3a5e2682c1afa1f767e884cc04
brandonc91 <brandonc91@gmail.com>
2014-12-12 15:33:58 -0800

    Working main menu for existing levels, level select not implemented yet, in-game pause menu, win conditions not set yet

---
+++ TopDownShooter/Assets/Scripts/GUIScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour
+{
+
+		public GUIStyle headerStyle;
+		public GUISkin sciFiSkin;
+		public int selectedLevel = -1;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	
+		void OnGUI ()
+		{
+				GUI.skin = sciFiSkin;
+				int xOffset = (Screen.width - 300) / 2;
+				int yOffset = (Screen.height - 175) / 2;
+		
+				Rect pos = new Rect (xOffset, yOffset, 300, 25);
+				if (GUI.Button (pos, "Play Game")) {
+						Debug.Log ("Clicked the Play Game button");
+						Application.LoadLevel (1);
+				}
+				pos.y += 50;
+				if (GUI.Button (pos, "Choose Level")) {
+						Debug.Log ("Clicked the choose level button");
+				}
+		
+		}
+
+//	void OnGUI() {
+//		int xOffset = (Screen.width - 600) / 2;
+//		int yOffset = (Screen.height - 400) / 2;
+//
+//		Rect pos = new Rect( xOffset, yOffset, 600, 50 );
+//		GUI.Label( pos, "Choose Level", headerStyle );
+//
+//		pos.y += 50;
+//		pos.height = 350;
+//		GUI.Box( pos, "" );
+//
+//		int x;
+//		int y;
+//		int levelNumber = 1;
+//		for( y = 0; y < 3; y++ ) {
+//			for( x = 0; x < 4; x++ ) {
+//				pos.x = xOffset + 125 + x * 100;
+//				pos.y = yOffset + 50 + 50 + y * 100;
+//				pos.width = pos.height = 50;
+//
+//				if( GUI.Button( pos, levelNumber.ToString() )) {
+//					selectedLevel = levelNumber;
+//				}
+//				levelNumber++;
+//			}
+//		}
+//
+//		pos.x = xOffset + 0;
+//		pos.y = yOffset + 400;
+//		pos.width = 600;
+//		pos.height = 20;
+//		GUI.Label( pos, "Selected Level is " + selectedLevel );
+//	}
+}
---
+++ TopDownShooter/Assets/Scripts/MoveBullet2.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBullet2 : MonoBehaviour {
+	public float speed;
+	private int death = 120;		//timer for bullet destroy: 3 seconds
+	
+	void Start () {
+		if(Input.GetAxis("Forward2") > 0)
+			transform.Translate (new Vector3(0,0,speed+1));
+	}
+	
+	
+	void Update () {
+		death--;
+		//this might work as game mechanic (sidenote)
+		//transform.RotateAround (transform.position, Vector3.up, 45);
+		transform.Translate (new Vector3(0,0,speed));
+		if (death <= 0) 
+			Destroy (this.gameObject);
+	}
+	
+	void OnTriggerEnter(Collider hit) 
+	{
+		if (hit.gameObject.name == "Cube") {
+			Destroy (this.gameObject);
+		}
+	}
+	
+
+	
+}
